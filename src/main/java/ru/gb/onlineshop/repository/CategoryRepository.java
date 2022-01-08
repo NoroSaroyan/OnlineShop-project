074bc199045637ef1c12a7f77945981b28f929e7
noriksaroyan <noriksaroyan@gmail.com>
2022-01-09 02:30:37 +0300

    password encoder bean is working properly

--- src/main/java/ru/gb/onlineshop/repository/CategoryRepository.java
+++ src/main/java/ru/gb/onlineshop/repository/CategoryRepository.java
@@ -1,13 +1,10 @@
 package ru.gb.onlineshop.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.gb.onlineshop.entity.Category;
 
 @Repository
-public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
-//    List<Category> findAll();
-//
-//    Category findById(long id);
-
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
